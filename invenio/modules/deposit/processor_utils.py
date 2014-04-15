c7144a6eb84d2c1d22bd21f081d7fdbf50bc497a
Pedro Gaudêncio <pedro.gaudencio@cern.ch>
2014-04-15 18:02:36 +0200

    deposit: addition of api for crossref doi fetch
    * This searches the DOI on crossref and returns
      the result as JSON format, also handles if the
      DOI isn't found or it's malformed.
    
    Signed-off-by: Pedro Gaudêncio <pedro.gaudencio@cern.ch>
    Reviewed-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- invenio/modules/deposit/processor_utils.py
+++ invenio/modules/deposit/processor_utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2014 CERN.
+## Copyright (C) 2013, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -70,7 +70,9 @@ class PidSchemeDetection(object):
 
 
 class PidNormalize(object):
+
     """ Normalize a persistent identifier. """
+
     def __init__(self, scheme_field=None):
         self.scheme_field = scheme_field
 
