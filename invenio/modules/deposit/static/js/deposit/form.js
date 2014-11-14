3463e326e36a07dbfaf9e8b73cb97a0dcabf52ee
Pedro Gaudêncio <pedro.gaudencio@cern.ch>
2014-11-14 08:22:07 +0200

    deposit: checkbox support addition
    * Saves the checkbox values on changed.
    
    * Corrects FileUploadField RequiredIf validation.
    
    Signed-off-by: Pedro Gaudêncio <pedro.gaudencio@cern.ch>
    Co-authored-by: Javier Martin Montull <javier.martin.montull@cern.ch>
    Reviewed-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- invenio/modules/deposit/static/js/deposit/form.js
+++ invenio/modules/deposit/static/js/deposit/form.js
@@ -109,7 +109,8 @@ define(function(require, exports, module) {
         });
       }
       var fields = $(selector).serializeArray(),
-          $pluploader = $(this.attr.pluploaderClass);
+          $pluploader = $(this.attr.pluploaderClass),
+          $checkboxes = $('input[type=checkbox]:not(:checked)');
 
       if ( $pluploader.length ) {
         // There is a pluploader instance in the form
@@ -117,6 +118,15 @@ define(function(require, exports, module) {
           name: 'files',
           value: $pluploader.data('pluploadWidget').val()
         });
+      }
+
+      if ( $checkboxes.length ) {
+        fields = fields.concat(
+          $checkboxes.map(
+              function() {
+                return {name: this.name, value: ''}
+              }).get()
+        );
       }
       return serialize_object(fields);
   }
@@ -586,6 +596,16 @@ define(function(require, exports, module) {
           }
   }
 
+  this.onCheckboxChanged = function (event) {
+    if(event.target.name.indexOf('__input__') == -1){
+      if ( $(event.target).prop("checked") ) {
+        save_field(this.attr.save_url, event.target.name, event.target.value);
+      } else {
+        save_field(this.attr.save_url, event.target.name, '');
+      }
+    }
+  }
+
   /**
    * Click form-button
    */
@@ -768,7 +788,8 @@ define(function(require, exports, module) {
     });
 
     this.on(this.attr.formSelector + ' .form-button', "click", this.onButtonClick);
-    this.on('#submitForm input, #submitForm textarea, #submitForm select', "change", this.onFieldChanged);
+    this.on('#submitForm input[type!=checkbox], #submitForm textarea, #submitForm select', "change", this.onFieldChanged);
+    this.on('#submitForm input[type=checkbox]', 'change', this.onCheckboxChanged);
 
     init_autocomplete('[data-autocomplete="1"]', this.attr.save_url, this.attr.autocomplete_url);
     init_field_lists(this.attr.formSelector + ' .dynamic-field-list', this.attr.save_url, '[data-autocomplete="1"]', this.attr.autocomplete_url);
